e09d5cb65a98c7e03e49c8129f63e8238e437f03
茹孟凯 <rumengkai@aliyun.com>
2020-03-25 21:38:08 +0800

    0325

---
+++ 7.整数反转.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=javascript
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// 首先想到的方法，转成字符串，然后翻转，最后转成number输出
+// var reverse = function(x) {
+//     if (x>0) {
+//         x += ''
+//         x = x.split('')
+//         x = x.reverse()
+//         x = x.join('')
+//         x *= 1
+//     } else if(x<0){
+//         x *= -1
+//         x += ''
+//         x = x.split('')
+//         x = x.reverse()
+//         x = x.join('')
+//         x *= -1
+//     } else {
+//         return 0
+//     }
+//     if (x>Math.pow(2,31)-1||x<-Math.pow(2,31)) {
+//         return 0
+//     } else {
+//         return x
+//     }
+// };
+// 还是下面这个写的优雅，但是效率低，
+// var reverse = function(x) {
+//     const border = 2**31
+//     const max = border - 1
+//     const min = -border
+
+//     const result = (x > 0 ? 1 : -1) * String(x).split('').filter(x => x !== '-').reverse().join('')
+//     return result > max || result < min ? 0 : result
+// };
+
+var reverse = function(x) {
+  let fh = "",
+    re;
+  if (x < 0) {
+    fh = "-";
+    x = 0 - x;
+  }
+  re = String(x)
+    .split("")
+    .reverse()
+    .join("");
+  if (
+    re.length > 10 ||
+    (re.length === 10 && re > (fh === "-" ? "2147483648" : "2147483647"))
+  ) {
+    return 0;
+  } else {
+    return fh + re;
+  }
+};
+
+// @lc code=end
---
+++ 8.字符串转换整数-atoi.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=8 lang=javascript
+ *
+ * [8] 字符串转换整数 (atoi)
+ */
+
+// @lc code=start
+/**
+ * @param {string} str
+ * @return {number}
+ */
+// var myAtoi = function (str) {
+//     var symbol = 1;
+//     var num = 0;
+//     var accepted = 0;
+
+//     for (var i = 0; i < str.length; i++) {
+//         if (/\d/.test(str[i])) {
+//             //数字
+//             accepted = 1;
+
+//             var tmp = Number(str[i]);
+
+//             if (num > 0x0CCCCCCC || (num === 0x0CCCCCCC && tmp > 7)) {
+//                 //正溢出
+//                 return 0x7FFFFFFF;
+//             }
+
+//             if (num < -0x0CCCCCCC || (num === -0x0CCCCCCC && tmp > 8)) {
+//                 //负溢出
+//                 return -0x80000000;
+//             }
+
+//             num = (num << 3) + (num << 1) + symbol * tmp;
+//             continue;
+//         } else if (accepted) {
+//             //其他字符，一旦开始接收，则全部停止
+//             break;
+//         }
+
+//         //初次遇到各种字符
+//         if (str[i] === ' ') {
+//             //空格，不理会
+//             continue;
+//         } else if (str[i] === '+') {
+//             //+号，开始接收
+//             accepted = 1;
+//             continue;
+//         } else if (str[i] === "-") {
+//             //-号，开始接收
+//             accepted = 1;
+//             symbol = -1;
+//             continue;
+//         }
+//         //其他任意字符第一次遇到直接返回
+//         return 0;
+//     }
+//     return num;
+// };
+
+var myAtoi = function(str) {
+    str = str.trim();
+    if(!/^[+|-]?\d+/.test(str)) return 0;
+    let val = parseInt(str.match(/^[+|-]?\d+/));
+    let base = Math.pow(2,31)
+    let min = -base;
+    let max = base-1;
+    return Math.max(Math.min(val, max), min)
+};
+
+// @lc code=end
+
