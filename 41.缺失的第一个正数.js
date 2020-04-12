aa75bb70d81f5d50a342f87994dff3cd8a122a77
茹孟凯 <rumengkai@aliyun.com>
2020-04-12 17:20:00 +0800

    a

--- 41.缺失的第一个正数.js
+++ 41.缺失的第一个正数.js
@@ -9,11 +9,33 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var firstMissingPositive = function (nums) {
+//   let i = 1;
+//   while (nums.indexOf(i) > -1) {
+//     i++;
+//   }
+//   return i;
+// };
 var firstMissingPositive = function (nums) {
-  let i = 1;
-  while (nums.indexOf(i) > -1) {
-    i++;
+  var n = nums.length;
+
+  //处理负数
+  //可以不处理超出 length+1 的值， 因为js数组没有越界问题
+  //由于 NaN 会影响判断 并且 结果最大为 length+1， 所以将边界增加到 length+1
+  //可以不添加边界，在结果判断NaN
+  for (var i = 0; i <= n + 1; i++) {
+    if (nums[i] <= 0 || !nums[i]) nums[i] = n + 1;
   }
-  return i;
+
+  //如果出现正整数 a，将第 a 项的值转成负数，标记 a 出现过
+  for (var i = 0; i < n; i++) {
+    var a = Math.abs(nums[i]);
+    nums[a] = -Math.abs(nums[a]);
+  }
+
+  //如果第 i 项为正整数，则说明 i 没有出现过（负数说明出现过），即为答案
+  for (var i = 1; i <= n + 1; i++) {
+    if (nums[i] > 0) return i;
+  }
 };
 // @lc code=end
